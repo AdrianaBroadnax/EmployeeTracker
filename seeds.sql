6e4ae2aaa66a81fa9c544b9a839a38ce5dbc13ef
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-01-01 12:15:30 -0500

    package.json install inquirer mysql2 save console.table

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (id, name)
+VALUES (001, "Arthur"),
+       (002, "Chinua"),
+       (003, "Margaret"),
+       (004, "Gabriel"),
+       (005, "Simone");
+       
+INSERT INTO role (id, role_id, title, salary)
+VALUES (001, 001, "coffee, milk"),
+       (002, 002, "eggs, cheese"),
+       (003, 003, "organic cereal"),
+       (004, 004, "oranges, peaches"),
+       (005, 005, "ice cream");
+INSERT INTO employee (id, role_id, employee_id, first_name, last_name)
+VALUES (001, )
