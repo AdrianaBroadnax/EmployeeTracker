60421de190eb92e600a00fe5f777b6c9b45d2c46
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-01-07 16:29:59 -0500

    index.js

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ const inquirer = require('inquirer')
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // TODO: Add MySQL password here
-    password: 'broadnax',
-    database: 'employeeTracker_db'
-    // user: process.env.DB_USER,
-    // // TODO: Add MySQL password here
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME
-  },
-  console.log(`Connected to the database.`)
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password here
+        password: 'broadnax',
+        database: 'employeeTracker_db'
+        // user: process.env.DB_USER,
+        // // TODO: Add MySQL password here
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME
+    },
+    console.log(`Connected to the database.`)
 );
 
 directory();
 
-function directory(){
+function directory() {
     inquirer.prompt([
         {
             type: "list",
@@ -38,59 +38,61 @@ function directory(){
             message: "what would you like to do",
             choices: [
                 {
-                    name:"view all departments",
-                    value:"view_departments"
+                    name: "view all departments",
+                    value: "view_departments"
                 },
                 {
-                    name:"view all roles",
-                    value:"view_roles"
+                    name: "view all roles",
+                    value: "view_roles"
                 },
                 {
-                    name:"view all employees",
-                    value:"view_employees"
+                    name: "view all employees",
+                    value: "view_employees"
                 },
                 {
-                    name:"add department",
-                    value:"add_department"
+                    name: "add department",
+                    value: "add_department"
                 },
                 {
-                    name:"add role",
-                    value:"add_role"
+                    name: "add role",
+                    value: "add_role"
                 },
                 {
-                    name:"add employee",
-                    value:"add_employee"
+                    name: "add employee",
+                    value: "add_employee"
                 },
                 {
-                    name:"update employee role",
-                    value:"update_employee role"
+                    name: "update employee role",
+                    value: "update_employee role"
                 }
 
             ]
         }
     ])
-    .then(answer => {
-        switch(answer.choice) {
-            case "view_departments":
-                viewDepartments();
-                break;
-            case "view_roles":
-                viewRoles();
-                break;
-//FINISH SWITCH CASE FUNCTIONS
-            case "view_employees":
-                viewEmployees();
-                break;
-            // default:
-            //     quit();
-        }
-    })
+        .then(answer => {
+            console.log(answer);
+            switch (answer.direction) {
+                case "view_departments":
+                    viewDepartments();
+                    break;
+                case "view_roles":
+                    viewRoles();
+                    break;
+                //FINISH SWITCH CASE FUNCTIONS
+                case "view_employees":
+                    viewEmployees();
+                    break;
+                default:
+                    console.error("oops!")
+                    // quit();
+            }
+        })
 
 
 }
 
 function viewDepartments() {
-    inquirer.prompt()[
+    inquirer.prompt([
         {
             type: "list",
             name: "department",
@@ -123,7 +125,24 @@ function viewDepartments() {
                 }
             ]
         }
-    ]
+    ])
+    .then(answer => {
+        console.log(answer);
+        switch (answer.department) {
+            case "view_hr":
+                viewHR();
+                break;
+            case "view_roles":
+                viewRoles();
+                break;
+            case "view_employees":
+                viewEmployees();
+                break;
+            default:
+                console.error("oops!")
+                // quit();
+        }
+    }) 
 }
 
 function viewRoles() {
